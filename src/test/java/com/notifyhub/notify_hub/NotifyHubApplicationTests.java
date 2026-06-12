d051b319d032190739f2172299f5d940b1a69cdd
Oğulcan Ocaklı <ogulcan.ocakli@mobiliva.com>
2026-06-12 11:41:07 +0300

    chore: initial project setup with docker-compose

---
+++ src/test/java/com/notifyhub/notify_hub/NotifyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifyhub.notify_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
